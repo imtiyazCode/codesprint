a2a1da3c0a77bc65637ccbec2b53cda981bcde5a
Imtiyaz <76092300+IntiTalent@users.noreply.github.com>
2022-08-20 10:43:35 +0530

    creating homepage banner

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,34 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Link from 'next/link';
+import { FaShoppingBag } from 'react-icons/fa'
 
 export default function Home() {
   return (
-    <div className="">
+    <div>
       <Head>
         <title>Codesprint</title>
         <meta name="description" content="Codesprint - Print And Wear the code" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <img src="/banner.png" alt="banner" className="w-full" />
+      <main className='font-poppins'>
+        <div className="banner bg-[#43485D] w-full h-fit py-7 md:py-0 md:h-[500px] flex items-center justify-center relative">
+          <div className="innerBanner bg-[#33384D] w-[90%] m-auto h-fit py-8 md:py-0 sm:h-[80%] md:h-[65%] lg:h-[75%] rounded-[10px] md:pl-[70px] md:flex md:justify-between">
+            <div className="headerText flex items-center justify-center md:items-start flex-col md:space-y-6">
+              <h1 className='text-[55px] sm:text-[75px] md:text-[70px] lg:text-[9vw] xl:text-[117px] font-extrabold text-white leading-[129px]' >
+                Codesprint
+              </h1>
+              <Link href="/hoodies">
+                <button className=' bg-white py-2 px-3 md:py-[12px] md:px-[20px] lg:py-[14px] lg:px-[24px] rounded-[42px] font-medium flex items-start md:text-[20px] text-[#33384D] hover:bg-[#43485D] hover:text-white'>
+                  SHOP NOW
+                  <FaShoppingBag className='my-auto ml-4' />
+                </button>
+              </Link>
+            </div>
+            <div className="imgWrapper hidden md:block md:min-h-full overflow-hidden">
+              <img src="/bannerPng.png" className='h-4 md:h-full' />
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      'poppins': ['Poppins', 'sans-serif']
+    }
   },
   plugins: [],
 }
