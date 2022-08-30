b7a41474810e66c62ddc76afee7b1e11892589af
Imtiyaz <76092300+IntiTalent@users.noreply.github.com>
2022-08-30 22:15:21 +0530

    creating signup page design

--- pages/login.js
+++ pages/login.js
@@ -5,7 +5,7 @@ import { AiFillLock } from 'react-icons/ai'
 const Login = () => {
   return (
     <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-gray-100">
-      <div className="max-w-md w-full space-y-8">
+      <div className="max-w-md w-full space-y-8 mb-8">
         <div>
           <img className="mx-auto h-12 w-auto" src="/logo.png" alt="logo" />
           <h2 className="mt-6 text-center text-3xl tracking-tight font-bold text-gray-900">Sign in to your account</h2>
