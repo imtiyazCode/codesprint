2136805342af1330e451546cd26d7a93629df019
Imtiyaz <76092300+IntiTalent@users.noreply.github.com>
2022-08-31 22:20:32 +0530

    creating product schema model

---
+++ models/Product.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const productSchema = new Schema({
+    title: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    desc: { type: String, required: true },
+    img: { type: String, required: true },
+    category: { type: String, required: true },
+    size: { type: String },
+    color: { type: String },
+    price: { type: Number, required: true },
+    availableQty: { type: Number, required: true }
+}, { timestamps: true });
+
+const Product = model('Product', productSchema)
+
+export default Product;
