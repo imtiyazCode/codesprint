1f13312fc407b1a597ef97731fb8d3bf0cd693ce
Imtiyaz <76092300+IntiTalent@users.noreply.github.com>
2022-08-31 22:16:21 +0530

    install mongoose and creating order schema model

---
+++ models/Order.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const orderSchema = new Schema({
+    userId: { type: String, required: true },
+    products: [{
+        productId: { type: String },
+        quantity: { type: Number, default: 1 }
+    }],
+    address: { type: String, required: true },
+    amount: { type: Number, required: true },
+    status: { type: String, default: 'Pending' }
+}, { timestamps: true });
+
+const Order =  model('Order', orderSchema)
+
+export default Order;
