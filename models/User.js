9eb7d040b257e293688522040341331209780a0c
Imtiyaz <76092300+IntiTalent@users.noreply.github.com>
2022-08-31 22:25:06 +0530

    creating user schema mode

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+}, { timestamps: true });
+
+const User = model('User', userSchema)
+
+export default User;
