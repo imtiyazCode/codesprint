38efa6430d0ca8dec221e4711d0433e8be516459
Imtiyaz <76092300+IntiTalent@users.noreply.github.com>
2022-10-03 22:38:08 +0530

    implementing signup fronend form

--- pages/signup.js
+++ pages/signup.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { AiFillLock } from 'react-icons/ai'
 
 const Signup = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [number, setNumber] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleChange = (e) => {
+    if (e.target.name == 'name') {
+      setName(e.target.value)
+    } else if (e.target.name == 'email') {
+      setEmail(e.target.value)
+    } else if (e.target.name == 'phone') {
+      setNumber(e.target.value)
+    } else if (e.target.name == 'password') {
+      setPassword(e.target.value)
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = { name, email, number, password };
+    let res = await fetch("http://localhost:3000/api/signup", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+    let data = await res.json()
+
+    setName('')
+    setEmail('')
+    setNumber('')
+    setPassword('')
+  }
+
   return (
     <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-gray-100">
       <div className="max-w-md w-full space-y-8 mb-8">
@@ -16,26 +52,26 @@ const Signup = () => {
             </Link>
           </p>
         </div>
-        <form className="mt-8 space-y-6 bg-white p-10 rounded-lg shadow" action="#" method="POST">
+        <form onSubmit={handleSubmit} className="mt-8 space-y-6 bg-white p-10 rounded-lg shadow" action="#" method="POST">
           <div className="rounded-md shadow-sm space-y-5">
             <div>
               <label htmlFor="name" className="">Name</label>
-              <input id="name" name="name" type="text" autoComplete="name" required
+              <input onChange={handleChange} id="name" name="name" type="text" autoComplete="name" required value={name}
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 text-gray-900 rounded-t-md focus:outline-none focus:ring-purple-500 focus:border-purple-500 focus:z-10 sm:text-sm my-1" />
             </div>
             <div>
               <label htmlFor="email-address" className="">Email address</label>
-              <input id="email-address" name="email" type="email" autoComplete="email" required
+              <input onChange={handleChange} id="email-address" name="email" type="email" value={email} autoComplete="email" required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 text-gray-900 rounded-t-md focus:outline-none focus:ring-purple-500 focus:border-purple-500 focus:z-10 sm:text-sm my-1" />
             </div>
             <div>
               <label htmlFor="phone" className="">Mobile Number</label>
-              <input id="phone" name="phone" type="number" autoComplete="phone"
+              <input onChange={handleChange} id="phone" name="phone" type="number" value={number} autoComplete="phone"
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 text-gray-900 rounded-t-md focus:outline-none focus:ring-purple-500 focus:border-purple-500 focus:z-10 sm:text-sm my-1" />
             </div>
             <div>
               <label htmlFor="password" className="">Password</label>
-              <input id="password" name="password" type="password" autoComplete="current-password" required
+              <input onChange={handleChange} id="password" name="password" type="password" value={password} autoComplete="current-password" required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 text-gray-900 rounded-t-md focus:outline-none focus:ring-purple-500 focus:border-purple-500 focus:z-10 sm:text-sm my-1" />
             </div>
           </div>
