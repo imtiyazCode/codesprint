8bb160425cf25e2d993084be3e60814ce46a4d93
Imtiyaz <76092300+IntiTalent@users.noreply.github.com>
2022-10-03 21:43:56 +0530

    creating signup api

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const { Schema, model, models } = mongoose;
 const userSchema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    number: { type: Number, required: true },
     password: { type: String, required: true }
 }, { timestamps: true });
 
---
+++ pages/api/signup.js
@@ -0,0 +1,14 @@
+import User from "../../models/User";
+import connectDB from "../../middleware/mongoose";
+
+const handler = async (req, res) => {
+    if (req.method === 'POST') {
+        let user = await User.create(req.body)
+        res.status(200).json({ flag: true })
+
+    } else {
+        res.status(400).json({ error: "This method is not allowed" })
+    }
+}
+
+export default connectDB(handler)
